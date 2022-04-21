710eb3d509f86e7793d94d8c1aadd241d77662e1
KODE LLC <vs@kode.ru>
2022-04-21 11:21:43 +0200

    test(utils): add tests

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-export { createTemplateRegExp } from './create-reg-exp';
+export { createTemplateRegExp } from './create-template-reg-exp';
 export { createUrl } from './create-url';
 export { parseUrl } from './parse-url';
 export { getPath } from './get-path';
