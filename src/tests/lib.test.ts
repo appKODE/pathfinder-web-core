f9b1887dd0167c9a1345abeb1b78b26a51f3739b
KODE LLC <vs@kode.ru>
2022-05-04 18:16:49 +0200

    test(utils): add tests for utils

--- src/tests/lib.test.ts
+++ src/tests/lib.test.ts
@@ -1,10 +1,3 @@
-import { createPathFinder } from '../lib';
-import { dataResolver, dataStorage } from './mocks';
-
 describe('createPathFinder test', () => {
-  it.todo('Should return correct pathfinder instance');
-  // it('should return correct result', () => {
-  //   const result = createPathFinder(dataResolver, dataStorage);
-  //   expect(result).toEqual('result');
-  // });
+  it.todo('Add tests for createPathFinder');
 });
