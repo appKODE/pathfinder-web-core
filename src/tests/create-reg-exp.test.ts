ca4e4b29b846041e8d2630678ff07b4c5ad0877e
ku <ku@kode.ru>
2022-05-04 18:16:49 +0200

    test(utils): add tests for utils

--- src/tests/create-reg-exp.test.ts
+++ src/tests/create-reg-exp.test.ts
@@ -2,12 +2,12 @@ import { createTemplateRegExp } from '../utils/create-template-reg-exp';
 import { endpointTemplate, endpointTemplateMatcher } from './mocks';
 
 describe('getPath test', () => {
-  it('should return correct result 1', () => {
+  it('should return correct result for normal endpoint template', () => {
     const result = createTemplateRegExp(endpointTemplate);
     expect(result).toEqual(endpointTemplateMatcher);
   });
 
-  it('should return correct result 2', () => {
+  it('should return non match regexp for empty template', () => {
     const result = createTemplateRegExp('');
     expect(result).toEqual(/(?:)/gm);
   });
