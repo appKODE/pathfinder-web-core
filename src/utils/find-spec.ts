9765c24442e2158a39c7c6a8d32047a62b32904f
ku <ku@kode.ru>
2022-07-11 11:00:04 +0200

    test(find-spec): add tests

--- src/utils/find-spec.ts
+++ src/utils/find-spec.ts
@@ -3,11 +3,21 @@ import { FindSpecFn, UrlSpec } from '../types';
 export const findSpec: FindSpecFn = (matchers, method, url) => {
   let result: UrlSpec | null = null;
 
-  matchers.forEach((value, key) => {
+  const { pathname } = new URL(url);
+
+  const pathnameChunks = pathname.split('/').length;
+
+  matchers.forEach((value, matcher) => {
+    const templateChunks = matcher.template.split('/').length;
+
     const match = url.match(value);
 
-    if (match && key.method === method) {
-      result = key;
+    if (
+      match &&
+      matcher.method === method &&
+      templateChunks === pathnameChunks
+    ) {
+      result = matcher;
     }
   });
 
